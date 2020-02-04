c7e0c45f768bb6a8beebafd4c9a6e002b82ceb6b
doramart <doramart@qq.com>
2020-02-04 12:35:00 +0800

    update egg-dora-versionmanage

--- app/controller/api/versionManage.js
+++ app/controller/api/versionManage.js
@@ -2,13 +2,42 @@
  * @Author: doramart 
  * @Date: 2019-09-23 14:44:21 
  * @Last Modified by: doramart
- * @Last Modified time: 2019-09-23 14:55:52
+ * @Last Modified time: 2019-11-15 16:36:14
  */
 
 
 
 let VersionManageController = {
 
+
+    /**
+     * @api {get} /api/versionManage/getAppVersion 获取APP版本信息
+     * @apiDescription 获取APP版本信息
+     * @apiName /versionManage/getAppVersion
+     * @apiGroup VersionManage
+     * @apiParam {string} versionCode 版本号(传字符串类型整数)
+     * @apiParam {string} client 客户端类型(0:安卓，1:ios)
+     * @apiSuccess {json} result
+     * @apiSuccessExample {json} Success-Response:
+     *{
+     *    "status": 200,
+     *    "message": "get versionData success",
+     *    "server_time": 1548049855934,
+     *    "data": {
+     *         "_id": "H158NRt7Q",
+     *         "description": "新增自动更新1",
+     *         "title": "有更新啦1",
+     *         "url": "/upload/images/apk20180717192615.apk",
+     *         "version": "21",
+     *         "versionName": "1.1",
+     *         "date": "2018-07-16 16:16:17",
+     *         "forcibly": true,
+     *         "id": "H158NRt7Q"
+     *    }
+     *}
+     * @apiSampleRequest http://localhost:8080/api/versionManage/getAppVersion
+     * @apiVersion 1.0.0
+     */
     async list(ctx) {
 
         try {
@@ -43,7 +72,6 @@ let VersionManageController = {
 
         }
     },
-
 
     async getOne(ctx) {
 
